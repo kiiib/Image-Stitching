67e7597e89bc0baa70f729f11df2b411b2261c7f
Jian-Bin Liu <billyljb@gmail.com>
2017-05-07 00:31:17 +0800

    update

--- Resource/Image Stitching/Image Stitching/main.cpp
+++ Resource/Image Stitching/Image Stitching/main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
 #include <opencv2\opencv.hpp>
 using namespace cv;
+using namespace std;
 
 int main() {
 	Mat img = imread("1.jpg");
-	imshow("aa", img);
+	Mat grayImg;
+	cvtColor(img, grayImg, CV_BGR2GRAY);
+	cout << grayImg.at<uchar> (1, 5) << endl;
+	cornerHarris(grayImg, grayImg, );
+	imshow("aa", grayImg);
 	waitKey();
+
+	return 0;
 }
